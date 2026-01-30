62d6adeec926c853194ba19f09c197d1369fcc16
Adrian Stachura <48847246+AdrianHStachura@users.noreply.github.com>
2026-01-29 22:16:11 -0500

    Add person struct and include libraries and config
    Added person struct and included necessary headers.

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include "libraries.h"
+#include "config.h"
+
+struct person
+{
+    char* fullName;
+    char* accountNumber;
+    int creditScore;
+    int currentBalance;
+    
+};
 
 int main(void){
+   system("clear");
     printf("Welcome\n");
+    sleep(2);
+    system("clear");
     return 0;
 }
